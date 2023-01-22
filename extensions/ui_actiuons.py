e37dfea65b7165cf0310ca738964b91e9b19a049
barkadosh <57194377+barkadosh@users.noreply.github.com>
2023-01-22 15:39:32 +0200

    Setup 22.1.23

--- extensions/ui_actiuons.py
+++ extensions/ui_actiuons.py
@@ -1,29 +1,31 @@
+from selenium.webdriver.remote.webelement import WebElement
+
 import test_cases.conftest as conf
 
 
 class UiActions:
     @staticmethod
-    def click(elem):
+    def click(elem: WebElement):
         #explistly wait...
         elem.click()
 
     @staticmethod
-    def update_text(elem, value):
+    def update_text(elem: WebElement, value: str):
         elem.send_keys(value)
 
     @staticmethod
-    def mouse_hover(elem1, elem2):
+    def mouse_hover(elem1: WebElement, elem2: WebElement):
         conf.action.move_to_element(elem1).move_to_element(elem2).perform()
 
     @staticmethod
-    def right_click(elem):
+    def right_click(elem: WebElement):
         conf.action.context_click(elem).perform()
 
     @staticmethod
-    def drag_and_drop(elem1, elem2):
+    def drag_and_drop(elem1: WebElement, elem2: WebElement):
         conf.action.drag_and_drop(elem1, elem2).perform()
 
     @staticmethod
-    def clear(elem):
+    def clear(elem: WebElement):
         elem.clear()
 
