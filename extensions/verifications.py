c800d931f3c29253a9baf78e478ba56b874fa932
barkadosh <57194377+barkadosh@users.noreply.github.com>
2023-01-22 15:56:04 +0200

    Setup 22.1.23

---
+++ extensions/verifications.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.remote.webelement import WebElement
+
+
+class Verifications:
+    @staticmethod
+    def verify_equals(actual, expected):
+        assert actual == expected, f'Verify Equals Failed, Actual: {str(actual)} is notequal to Expected: {str(actual)}'
+
+    @staticmethod
+    def is_displayed(elem: WebElement):
+        assert elem.is_displayed(), f'Verify Is Displayed Failed, Element: {elem.text} is not displayed'
+
