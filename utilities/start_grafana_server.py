90b89b7499be614c5b503bff941238688fae3248
barkadosh <barkadosh@gmail.com>
2023-04-25 11:46:27 +0300

    25.4.23

--- utilities/start_grafana_server.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-import time
-
-process = subprocess.Popen('C:\\Automation\\grafana-enterprise-9.3.2.windows-amd64\\grafana-9.3.2\\bin\\grafana-server.exe --homepath=C:\\Automation\\grafana-enterprise-9.3.2.windows-amd64\\grafana-9.3.2')
-time.sleep(5)
-process.terminate()
