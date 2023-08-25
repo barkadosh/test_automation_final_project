e226c1bc2edf79e56a56ff5c672004adffd84089
barkadosh <barkadosh@gmail.com>
2023-08-25 19:30:27 +0300

    Editing mobile test cases 25.8.23

--- test_cases/test_mobile.py
+++ test_cases/test_mobile.py
@@ -1,13 +1,10 @@
+import time
+
 import allure
 
 import pytest
 from utilities.enums import Save, Direction
-from workflows import mobile_flows
 from workflows.mobile_flows import MobileFlows
-
-
-#
-
 
 @pytest.mark.usefixtures('init_mobile_driver')
 class TestMobile:
@@ -15,7 +12,7 @@ class TestMobile:
     @allure.description("This test verify that calculated transaction that are not saved"
                         " won't appear in the saved transaction")
     @pytest.mark.sanity
-    # @pytest.mark.xfail
+    @pytest.mark.xfail
     def test_verify_mortgage_repayment(self):       # This test will fail
         MobileFlows.mortgage_flow('1000', '5', '2.5', Save.NO)
         MobileFlows.verify_mortgage_repayment('17.94')
@@ -30,13 +27,10 @@ class TestMobile:
         MobileFlows.verify_rate('3.0')
 
     @allure.title("TC03: Delete saved transaction")
-    @allure.description("this test delete the saved transaction and verify it deleted")
+    @allure.description("this test verify transaction is deleted from tear down method")
     @pytest.mark.sanity
     def test_delete_saved_trans(self):
-        MobileFlows.delete_saved_trans()
         MobileFlows.verify_trans_is_deleted('59.36')
-        MobileFlows.delete_saved_trans()
-
 
     # ~~~ My test cases ~~~
 
@@ -44,20 +38,18 @@ class TestMobile:
     @allure.description("this test Verify amount, yrs, percentage, repayment, interest are the same in the calculator "
                         "and in the saved transaction")
     @pytest.mark.run_this
-    def test_delete_saved_trans(self):
+    def test_mortgage_details(self):
+        MobileFlows.swipe_screen(Direction.RIGHT)
         MobileFlows.mortgage_flow('1000', '10', '10', Save.YES)
         MobileFlows.compare_transaction_details('1000', '10', '10')
 
-
-
-
-
-
-
+    def teardown_method(self):
+        time.sleep(1)
+        MobileFlows.delete_saved_trans()
 
 
 
 # Test 1: Verify amount, yrs, percentage, repayment, interest are the same in the calculator and in the saved transaction
-# Test 2: Verify that the date and houre the transaction is saved on are the current time
+# Test 2: Verify that the date and hour of the transaction is saved on are the current time
 # Test 3: Verify the calculation is correct = amount*(1+precetage)/yrs
 
